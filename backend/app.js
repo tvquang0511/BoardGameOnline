958f301ede22c481dec2901efe7a5ba85db84eca
23120346 <quangsexy01@gmail.com>
2026-01-20 22:48:51 +0700

    fix

--- backend/app.js
+++ backend/app.js
@@ -18,10 +18,13 @@ const app = express();
 app.use(helmet());
 app.use(cors({
   origin: [
-    "http://localhost:5173",
-    "https://vercel.com/vuquangs-projects-2943ee1c/board-game-online/GuR5YECWBWTxKoK2y8vJJgL9B4GQ",
+    "https://board-game-online-q61zso1oi-vuquangs-projects-2943ee1c.vercel.app",
+    "https://board-game-online.vercel.app", // nếu có prod
+    "http://localhost:5173"
   ],
-  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true
 }));
 
 
